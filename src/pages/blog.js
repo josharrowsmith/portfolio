e3b44d5ca22ad41fb556939c631e8d76e943ad2d
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2021-01-19 18:27:00 +1000

    blog coming along

---
+++ src/pages/blog.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+import PostGrid, { PostGridItem } from '../components/PostGrid'
+
+const HomePageGrid = styled.div`
+  display: grid;
+  justify-items: center;
+`
+
+function IndexPage({ data }) {
+  return (
+    <HomePageGrid>
+      <PostGrid>
+        {data.allMdx &&
+          data.allMdx.edges.map(function ({ node: post }) {
+            return (
+              <PostGridItem key={post.id}>
+                {post.frontmatter.image &&
+                  post.frontmatter.image.childImageSharp && (
+                    <div>
+                      <Link to={post.fields.slug}>
+                        <Img
+                          fluid={post.frontmatter.image.childImageSharp.fluid}
+                        />
+                      </Link>
+                      <div className="postMeta">
+                        <time dateTime={post.frontmatter.date} />
+                        <ul className="categories">
+                          {post.frontmatter.category.map(cat => (
+                            <li key={cat}>{cat}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  )}
+                <div>
+                  <h3>
+                    <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+                  </h3>
+                  <p>{post.excerpt}</p>
+                </div>
+              </PostGridItem>
+            )
+          })}
+      </PostGrid>
+    </HomePageGrid>
+  )
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query blogPosts($skip: Int! = 0) {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMdx(
+      filter: { fields: { collection: { eq: "post" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 10
+      skip: $skip
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          excerpt
+          fields {
+            collection
+            slug
+          }
+          frontmatter {
+            title
+            date
+            category
+            image {
+              ...ImageFields
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import styled from 'styled-components'
+
+const PostHeaderStyles = styled.header`
+  h1 {
+    margin-bottom: 0;
+  }
+  .postMeta {
+    font-size: 1.4rem;
+    font-style: italic;
+    display: grid;
+    grid-gap: 1rem;
+    grid-auto-flow: column;
+    justify-content: start;
+    & > * {
+      :not(:last-child):after {
+        content: '×';
+        margin-left: 1rem;
+        color: var(--yellow);
+        font-weight: 600;
+        font-style: normal;
+      }
+    }
+  }
+`
 
 export const pageQuery = graphql`
   query($slug: String!) {
@@ -18,6 +43,9 @@ export const pageQuery = graphql`
         slug
         date(formatString: "MMMM DD, YYYY")
         category
+        image {
+          ...ImageFields
+        }
       }
       body
     }
@@ -28,7 +56,14 @@ export default function Post({ data: { mdx: post }, scope, pageContext }) {
   console.log(post)
   return (
     <>
-      <h1>Post One</h1>
+      <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
+      <PostHeaderStyles>
+        <h1>{post.frontmatter.title}</h1>
+        <div className="postMeta">
+          <time dateTime={post.frontmatter.date}>{post.frontmatter.date}</time>
+          <span>{post.frontmatter.category.join(', ')}</span>
+        </div>
+      </PostHeaderStyles>
     </>
   )
 }
